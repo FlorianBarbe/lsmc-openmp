ae449754994d30df92a5b0f6d8e2b4f614231c10
Florian BARBE <florian.barbe34@gmail.com>
2025-10-22 17:07:43 +0200

    - ajout d'une interface utilisateur via le module pyhton UI.py - modifs de lsmc.hpp & main.cpp pour prendre les paramètres de l'option en entrée afin de les piloter avec les curseurs de l'UI. - Objectif : affichage sur l'UI des temps d'execution avec ou sans multithreading, graphe de tous les chemins simulés etc.

--- lsmc/lsmc.hpp
+++ lsmc/lsmc.hpp
@@ -1,7 +1,3 @@
-/**
- * lsmc.hpp
- * Implémentation de la méthode de Longstaff–Schwartz (LSMC)
- */
 #pragma once
 #include <vector>
 
@@ -9,4 +5,3 @@ namespace LSMC {
     double priceAmericanPut(double S0, double K, double r, double sigma,
         double T, int N_steps, int N_paths);
 }
-#pragma once
