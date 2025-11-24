5064a9bbfa78afcbecef713b72fb481da30cb99a
Florian Barbe <florian.barbe34@gmail.com>
2025-11-24 11:39:53 +0100

    Annotations pr décrire la classe gbm

--- include/gbm.hpp
+++ include/gbm.hpp
@@ -3,7 +3,24 @@
 #include <string>
 #include "rng.hpp"
 
+// -----------------------------------------------------------------------------
+// Classe GBM (Geometric Brownian Motion) qui regroupe les fonctions liées au mouvement brownien géométrique.
+// Elle sert à :
+//   1) simuler un ensemble de trajectoires du sous-jacent selon le modèle GBM,
+//   2) stocker ces trajectoires dans une matrice paths[ N_paths ][ N_steps+1 ],
+//   3) exporter proprement ces trajectoires dans un fichier CSV pour Streamlit.
+//
+// Elle fournit donc les données brutes nécessaires au LSMC.
+// -----------------------------------------------------------------------------
+
+
 class GBM {
+private:
+    double S0;
+    double r;
+    double sigma;
+    double T;
+    int N_steps;
 public:
     // Constructeur : paramètres du modèle
     GBM(double S0, double r, double sigma, double T, int N_steps);
@@ -21,11 +38,4 @@ public:
         const std::vector<std::vector<double>>& paths,
         const std::string& filename
     );
-
-private:
-    double S0;
-    double r;
-    double sigma;
-    double T;
-    int N_steps;
 };
